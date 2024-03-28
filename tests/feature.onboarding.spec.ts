284b281e41444298a132d3f89d025f8f03229c73
miguelangaranocurrents <miguel@currents.dev>
2024-03-27 23:30:20 -0500

    fix20

--- tests/feature.onboarding.spec.ts
+++ tests/feature.onboarding.spec.ts
@@ -11,7 +11,7 @@ test.describe(() => {
       contentType: "image/png",
     });
     const screenshot1 = await page.screenshot();
-    await testInfo.attach("screenshot14", {
+    await testInfo.attach("screenshot15", {
       body: screenshot,
       contentType: "image/png",
     });
